382f0397c2e71b70fa4bfa0a149181181797cff2
LuciaDelta3 <zorolaw3456@gmail.com>
2025-02-09 21:54:06 -0500

    Add Google AdSense code to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import Future from './pages/Future';
 
 function App() {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.async = true;
+    script.crossOrigin = 'anonymous';
+    script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8630572584569784';
+    document.head.appendChild(script);
+
+    return () => {
+      document.head.removeChild(script);
+    };
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
